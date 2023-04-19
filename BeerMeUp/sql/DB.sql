bd67dbaa20046c847b62021bb13785ab3063a0e0
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-04-19 18:39:01 +0200

    script db tradotto

--- BeerMeUp/sql/DB.sql
+++ BeerMeUp/sql/DB.sql
@@ -2,7 +2,7 @@ DROP SCHEMA IF EXISTS beer_me_up;
 CREATE SCHEMA beer_me_up;
 USE beer_me_up;
 
-CREATE TABLE utente(
+CREATE TABLE site_user(
 	id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     pw VARCHAR(255) NOT NULL,
@@ -12,20 +12,20 @@ CREATE TABLE utente(
     PRIMARY KEY(id)
 );
 
-CREATE TABLE indirizzo(
+CREATE TABLE address(
 	id INT NOT NULL AUTO_INCREMENT,
-    utente_id INT NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    cognome VARCHAR(255) NOT NULL,
-    telefono VARCHAR(15) NOT NULL,
-    via VARCHAR(255) NOT NULL,
-    civico VARCHAR(5) NOT NULL,
+    user_id INT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    telephone VARCHAR(15) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    num VARCHAR(5) NOT NULL,
     cap VARCHAR(5) NOT NULL,
-    citta VARCHAR(255) NOT NULL,
-    nazione VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    nation VARCHAR(255) NOT NULL,
     
     PRIMARY KEY(id),
-    FOREIGN KEY(utente_id) REFERENCES utente(id)
+    FOREIGN KEY(user_id) REFERENCES site_user(id)
 		ON DELETE CASCADE
         ON UPDATE CASCADE
 );
@@ -47,75 +47,75 @@ CREATE TABLE style(
     PRIMARY KEY(id)
 );
 
-CREATE TABLE birra(
+CREATE TABLE beer(
 	id INT NOT NULL AUTO_INCREMENT,
-    produttore_id INT NOT NULL,
-    stile_id INT NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    descrizione VARCHAR(1000) NOT NULL,
-    colore VARCHAR (255) NOT NULL,
-    ingredienti VARCHAR (255) NOT NULL,
-    gradazione DECIMAL(3,1) NOT NULL CHECK(gradazione>=0),
-    prezzo DECIMAL(9,2) NOT NULL CHECK(prezzo>0),
+    brewery_id INT NOT NULL,
+    style_id INT NOT NULL,
+    beer_name VARCHAR(255) NOT NULL,
+    beer_description VARCHAR(1000) NOT NULL,
+    color VARCHAR (255) NOT NULL,
+    ingredients VARCHAR (255) NOT NULL,
+    gradation DECIMAL(3,1) NOT NULL CHECK(gradation>=0 AND gradation<100),
+    price DECIMAL(9,2) NOT NULL CHECK(price>0),
     iva DECIMAL(3,1) NOT NULL CHECK(iva>=0),
     stock INT NOT NULL CHECK(stock>=0),
-    sconto INT NOT NULL CHECK(sconto>=0 AND sconto<100),
+    discount INT NOT NULL CHECK(discount>=0 AND discount<100),
     
     PRIMARY KEY(id),
-    FOREIGN KEY(produttore_id) REFERENCES brewery(id)
+    FOREIGN KEY(brewery_id) REFERENCES brewery(id)
 		ON DELETE CASCADE
         ON UPDATE CASCADE,
-	FOREIGN KEY(stile_id) REFERENCES style(id)
+	FOREIGN KEY(style_id) REFERENCES style(id)
 		ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
-CREATE TABLE recensione(
+CREATE TABLE review(
 	id INT NOT NULL AUTO_INCREMENT,
-    utente_id INT NOT NULL,
-    birra_id INT NOT NULL,
-    commenti VARCHAR(255) NOT NULL,
-    punteggio INT NOT NULL CHECK(punteggio>=1 AND punteggio<=5),
-    data_recensione DATE NOT NULL,
+    user_id INT NOT NULL,
+    beer_id INT NOT NULL,
+    comments VARCHAR(255) NOT NULL,
+    score INT NOT NULL CHECK(score>=1 AND score<=5),
+    review_date DATE NOT NULL,
     
     PRIMARY KEY (id),
-    FOREIGN KEY (utente_id) REFERENCES utente(id)
+    FOREIGN KEY (user_id) REFERENCES site_user(id)
 		ON DELETE CASCADE
         ON UPDATE CASCADE,
-	FOREIGN KEY (birra_id) REFERENCES birra(id)
+	FOREIGN KEY (beer_id) REFERENCES beer(id)
 		ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
-CREATE TABLE ordine(
+CREATE TABLE site_order(
 	id INT NOT NULL AUTO_INCREMENT,
-    utente_id INT NOT NULL,
-    indirizzo_spedizione VARCHAR(255) NOT NULL,
-    indirizzo_fatturazione VARCHAR(255) NOT NULL,
-    info_pagamento VARCHAR(255) NOT NULL,
-    stato VARCHAR(255) NOT NULL,
-    totale DECIMAL(9,2) NOT NULL CHECK(totale>0),
-    data_ordine DATE NOT NULL,
+    user_id INT NOT NULL,
+    shipping_address VARCHAR(255) NOT NULL,
+    billing_address VARCHAR(255) NOT NULL,
+    payment_info VARCHAR(255) NOT NULL,
+    order_status VARCHAR(255) NOT NULL,
+    total DECIMAL(9,2) NOT NULL CHECK(total>0),
+    order_date DATE NOT NULL,
     
     PRIMARY KEY(id),
-    FOREIGN KEY(utente_id) REFERENCES utente(id)
+    FOREIGN KEY(user_id) REFERENCES site_user(id)
 		ON DELETE NO ACTION
         ON UPDATE CASCADE
 );
 
-CREATE TABLE dettagli_ordine(
-	ordine_id INT NOT NULL,
-    birra_id INT NOT NULL,
-    descrizione VARCHAR(255) NOT NULL,
+CREATE TABLE order_details(
+	order_id INT NOT NULL,
+    beer_id INT NOT NULL,
+    des VARCHAR(255) NOT NULL,
     qta INT NOT NULL CHECK(qta>0),
     iva DECIMAL(3,1) NOT NULL CHECK(iva>=0),
-    prezzo DECIMAL(9,2) NOT NULL CHECK(prezzo>0),
+    price DECIMAL(9,2) NOT NULL CHECK(price>0),
     
-    PRIMARY KEY(ordine_id, birra_id),
-    FOREIGN KEY(ordine_id) REFERENCES ordine(id)
+    PRIMARY KEY(order_id, beer_id),
+    FOREIGN KEY(order_id) REFERENCES site_order(id)
 		ON DELETE CASCADE
         ON UPDATE CASCADE,
-	FOREIGN KEY(birra_id) REFERENCES birra(id)
+	FOREIGN KEY(beer_id) REFERENCES beer(id)
 		ON DELETE NO ACTION
         ON UPDATE CASCADE
 );
@@ -134,7 +134,7 @@ VALUES ('Stout' , 'Delicate, con un corpo medio ed una carbonatazione moderata, 
 con toni secondari di cacao o cereale torrefatto. 
 La cremosità e il tenue fruttato/luppolato donano equilibrio a queste birre.');
 
-INSERT INTO birra (produttore_id , stile_id, nome, descrizione, colore, ingredienti, gradazione, prezzo, iva, stock, sconto)
+INSERT INTO beer (brewery_id , style_id, beer_name, beer_description, color, ingredients, gradation, price, iva, stock, discount)
 VALUES ('1', '1', 'See Ya In The Pit', 'Dal colore ebano e impenetrabile, posta alla luce del sole mostra suggestivi riflessi rubini sotto una sottile nuvola di schiuma color cappuccino.
 La scorza d`arancia candita presente nella ricetta emerge all`olfatto in primissima battuta, accompagnata da note di liquirizia gommosa 
 e da una chiaro ricordo di caffè espresso con pennellate balsamiche e floreali che richiamano l`anice verde e la violetta.' ,
@@ -145,7 +145,7 @@ VALUES ('Pale Ale' , 'Originario della Gran Bretagna, ad oggi è uno dei princip
 Vede l’impiego di lievito ad alta fermentazione e malto prevalentemente chiaro che determina il colore dorato della birra. 
 Differenti tecniche di produzione e luppolature, con una vasta gamma di gusti e gradazioni alcoliche.');
 
-INSERT INTO birra (produttore_id , stile_id, nome, descrizione, colore, ingredienti, gradazione, prezzo, iva, stock, sconto)
+INSERT INTO beer (brewery_id , style_id, beer_name, beer_description, color, ingredients, gradation, price, iva, stock, discount)
 VALUES ('1', '2', 'Impact Zone', 'Dal colore dorato chiaro, opalescente, fa sfoggio di una schiuma candida, compatta e fine.
 Il bouquet presenta una sinergia tra sentori agrumati (mandarino e arancia tarocco), più calde evocazioni tropicali, 
 con il passion fruit in prima fila, e di matura frutta estiva che giungono fino alla pesca sciroppata.
@@ -153,7 +153,7 @@ Non manca anche una rinfrescante vena resinosa a precedere l`emersione in superf
 che si esprimono in uno sfondo di panificato chiaro punteggiato da un tocco di crosta di pan brioche.' ,
 'Dorato Chiaro' , 'Orzo', '5' , '4.50', '22', '10', '0' );
 
-select * from birra;
+select * from beer;
 select * from brewery;
 select * from style;
 
