49f6ed29ecee718d170362345465d2d771e65b12
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-05-12 17:47:49 +0200

    fix

--- BeerMeUp/src/it/beermeup/model/BeerDao.java
+++ BeerMeUp/src/it/beermeup/model/BeerDao.java
@@ -41,6 +41,45 @@ public class BeerDao{
 		}
 	}
 	
+	//Per chiudere tutte le risorse dopo una query
+	private void terminateQuery(PreparedStatement ps, Connection connection) throws SQLException {
+		try {
+			if(ps != null) {
+				ps.close();
+			}
+		}
+		finally {
+			if(connection != null) {
+				connection.close();
+			}
+		}
+	}
+	
+	private Beer getBeerFromRS(ResultSet rs) throws SQLException {
+		Beer bean = new Beer();
+		bean.setId(rs.getInt("id"));
+		bean.setBreweryId(rs.getInt("brewery_id"));
+		bean.setStyleId(rs.getInt("style_id"));
+		bean.setName(rs.getString("beer_name"));
+		bean.setDescription(rs.getString("beer_description"));
+		bean.setColor(rs.getString("color"));
+		bean.setIngredients(rs.getString("ingredients"));
+		bean.setGradation(rs.getBigDecimal("gradation"));
+		bean.setPrice(rs.getBigDecimal("price"));
+		bean.setIva(rs.getBigDecimal("iva"));
+		bean.setStock(rs.getInt("stock"));
+		bean.setDiscount(rs.getInt("discount"));
+		
+		try {
+			bean.setBase64Image(imgConvert(rs.getBlob("img")));
+		} 
+		catch (IOException e) {
+			BeerDao.logger.log(Level.WARNING, "Errore Lettura Immagine");
+		}
+		
+		return bean;
+	}
+	
 	private String imgConvert(Blob blob) throws IOException, SQLException {
 		InputStream inputStream = blob.getBinaryStream();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -60,6 +99,20 @@ public class BeerDao{
         return base64Image;
 	}
 	
+	private void buildBeerPS(Beer bean, PreparedStatement ps) throws SQLException {
+		ps.setInt(1, bean.getBreweryId());
+		ps.setInt(2, bean.getStyleId());
+		ps.setString(3, bean.getName());
+		ps.setString(4, bean.getDescription());
+		ps.setString(5, bean.getColor());
+		ps.setString(6, bean.getIngredients());
+		ps.setBigDecimal(7, bean.getGradation());
+		ps.setBigDecimal(8, bean.getPrice());
+		ps.setBigDecimal(9, bean.getIva());
+		ps.setInt(10, bean.getStock());
+		ps.setInt(11, bean.getDiscount());
+		ps.setInt(12, bean.getId());
+	}
 	
 	public synchronized Beer doRetrieveByKey(int id) throws SQLException {
 		Beer bean = new Beer();
@@ -73,41 +126,14 @@ public class BeerDao{
 			
 			ps = connection.prepareStatement(sql);
 			ps.setInt(1, id);
-			
 			rs = ps.executeQuery();
-			while(rs.next()) {
-				bean.setId(rs.getInt("id"));
-				bean.setBreweryId(rs.getInt("brewery_id"));
-				bean.setStyleId(rs.getInt("style_id"));
-				bean.setName(rs.getString("beer_name"));
-				bean.setDescription(rs.getString("beer_description"));
-				bean.setColor(rs.getString("color"));
-				bean.setIngredients(rs.getString("ingredients"));
-				bean.setGradation(rs.getBigDecimal("gradation"));
-				bean.setPrice(rs.getBigDecimal("price"));
-				bean.setIva(rs.getBigDecimal("iva"));
-				bean.setStock(rs.getInt("stock"));
-				bean.setDiscount(rs.getInt("discount"));
-				
-				try {
-					bean.setBase64Image(imgConvert(rs.getBlob("img")));
-				} 
-				catch (IOException e) {
-					BeerDao.logger.log(Level.WARNING, "Errore Lettura Immagine");
-				}
+			
+			while(rs.next()) {		
+				bean = getBeerFromRS(rs);
 			}		
 		}
 		finally {
-			try {
-				if(ps != null) {
-					ps.close();
-				}
-			}
-			finally {
-				if(connection != null) {
-					connection.close();
-				}
-			}
+			terminateQuery(ps, connection);
 		}
 		
 		return bean;
@@ -134,42 +160,12 @@ public class BeerDao{
 			
 			rs = ps.executeQuery();
 			while(rs.next()) {
-				Beer bean = new Beer();
-				
-				bean.setId(rs.getInt("id"));
-				bean.setBreweryId(rs.getInt("brewery_id"));
-				bean.setStyleId(rs.getInt("style_id"));
-				bean.setName(rs.getString("beer_name"));
-				bean.setDescription(rs.getString("beer_description"));
-				bean.setColor(rs.getString("color"));
-				bean.setIngredients(rs.getString("ingredients"));
-				bean.setGradation(rs.getBigDecimal("gradation"));
-				bean.setPrice(rs.getBigDecimal("price"));
-				bean.setIva(rs.getBigDecimal("iva"));
-				bean.setStock(rs.getInt("stock"));
-				bean.setDiscount(rs.getInt("discount"));
-				
-				try {
-					bean.setBase64Image(imgConvert(rs.getBlob("img")));
-				} 
-				catch (IOException e) {
-					BeerDao.logger.log(Level.WARNING, "Errore Lettura Immagine");
-				}
-				
+				Beer bean = getBeerFromRS(rs);
 				collection.add(bean);
 			}		
 		}
 		finally {
-				try {
-					if(ps != null) {
-						ps.close();
-					}
-				}
-				finally {
-					if(connection != null) {
-						connection.close();
-					}
-				}
+			terminateQuery(ps, connection);
 		}
 		
 		return collection;
@@ -186,33 +182,13 @@ public class BeerDao{
 			connection.setAutoCommit(false);
 			
 			ps = connection.prepareStatement(sql);
-			ps.setInt(1, bean.getBreweryId());
-			ps.setInt(2, bean.getStyleId());
-			ps.setString(3, bean.getName());
-			ps.setString(4, bean.getDescription());
-			ps.setString(5, bean.getColor());
-			ps.setString(6, bean.getIngredients());
-			ps.setBigDecimal(7, bean.getGradation());
-			ps.setBigDecimal(8, bean.getPrice());
-			ps.setBigDecimal(9, bean.getIva());
-			ps.setInt(10, bean.getStock());
-			ps.setInt(11, bean.getDiscount());
-			ps.setBinaryStream(12, bean.getInputStreamImage());
+			buildBeerPS(bean, ps);
 			
 			ps.executeUpdate();
 			connection.commit();		
 		}
 		finally {
-			try {
-				if(ps != null) {
-					ps.close();
-				}
-			}
-			finally {
-				if(connection != null) {
-					connection.close();
-				}
-			}
+			terminateQuery(ps, connection);
 		}
 		
 	}
@@ -225,36 +201,14 @@ public class BeerDao{
 		
 		try {
 			connection = ds.getConnection(); 
-			connection.setAutoCommit(false);
-			
+			connection.setAutoCommit(false);	
 			ps = connection.prepareStatement(sql);
-			ps.setInt(1, bean.getBreweryId());
-			ps.setInt(2, bean.getStyleId());
-			ps.setString(3, bean.getName());
-			ps.setString(4, bean.getDescription());
-			ps.setString(5, bean.getColor());
-			ps.setString(6, bean.getIngredients());
-			ps.setBigDecimal(7, bean.getGradation());
-			ps.setBigDecimal(8, bean.getPrice());
-			ps.setBigDecimal(9, bean.getIva());
-			ps.setInt(10, bean.getStock());
-			ps.setInt(11, bean.getDiscount());
-			ps.setInt(12, bean.getId());
-			
+			buildBeerPS(bean, ps);	
 			ps.executeUpdate();
 			connection.commit();		
 		}
 		finally {
-			try {
-				if(ps != null) {
-					ps.close();
-				}
-			}
-			finally {
-				if(connection != null) {
-					connection.close();
-				}
-			}
+			terminateQuery(ps, connection);
 		}
 	}
 
@@ -275,16 +229,7 @@ public class BeerDao{
 			connection.commit();		
 		}
 		finally {
-			try {
-				if(ps != null) {
-					ps.close();
-				}
-			}
-			finally {
-				if(connection != null) {
-					connection.close();
-				}
-			}
+			terminateQuery(ps, connection);
 		}
 		
 		return (result!=0);
